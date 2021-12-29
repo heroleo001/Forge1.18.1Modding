eaef5d9e9025399d918c6ce831db5a470f458ecb
heroleo001 <leo.heriban@gmail.com>
2021-12-29 18:36:40 +0100

    added a levitation sword

--- src/main/java/net/leo/herotech/item/ModItems.java
+++ src/main/java/net/leo/herotech/item/ModItems.java
@@ -4,7 +4,12 @@ import net.leo.herotech.HeroTech;
 import net.leo.herotech.item.custom.DrillItem;
 import net.leo.herotech.item.custom.ModTiers;
 import net.leo.herotech.item.custom.ScannerItem;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,6 +22,16 @@ public class ModItems {
 
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
             () -> new Item(new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)));
+
+    public static final RegistryObject<Item> COBALT_LEVITATION_SWORD = ITEMS.register("cobalt_levitation_sword",
+            () -> new SwordItem(ModTiers.COBALT, 2, 40f,
+                    new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)){
+                @Override
+                public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+                    pTarget.addEffect(new MobEffectInstance(MobEffects.LEVITATION, (60*20)));
+                    return super.hurtEnemy(pStack, pTarget, pAttacker);
+                }
+            });
 
     public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
             () -> new Item(new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)));
--- src/main/java/net/leo/herotech/item/custom/ModTiers.java
+++ src/main/java/net/leo/herotech/item/custom/ModTiers.java
@@ -1,16 +1,20 @@
 package net.leo.herotech.item.custom;
 
+import net.leo.herotech.item.ModItems;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier DRILL = new ForgeTier(2, 1000, 7f, 0f,
+    public static final ForgeTier DRILL = new ForgeTier(2, 1000, 9f, 0f,
             0, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.EMPTY);
 
-    public static final ForgeTier DIAMOND_DRILL = new ForgeTier(3, 1500, 10f, 0f,
+    public static final ForgeTier DIAMOND_DRILL = new ForgeTier(3, 1500, 16f, 0f,
             0, BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.EMPTY);
 
-    public static final ForgeTier ADVANCED_DIAMOND_DRILL = new ForgeTier(4, 2000, 13f, 0f,
+    public static final ForgeTier ADVANCED_DIAMOND_DRILL = new ForgeTier(4, 2000, 20f, 0f,
             0, BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.EMPTY);
+
+    public static final ForgeTier COBALT = new ForgeTier(2, 100, 10f, 2f, 0,
+            BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ModItems.COBALT_INGOT.get()));
 }
