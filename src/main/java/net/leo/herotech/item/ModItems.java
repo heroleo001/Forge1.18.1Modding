3a2fe445759931b0347d9329445e3f34565d03e1
heroleo001 <leo.heriban@gmail.com>
2021-12-29 18:01:55 +0100

    added advanced drills

--- src/main/java/net/leo/herotech/item/ModItems.java
+++ src/main/java/net/leo/herotech/item/ModItems.java
@@ -2,6 +2,7 @@ package net.leo.herotech.item;
 
 import net.leo.herotech.HeroTech;
 import net.leo.herotech.item.custom.DrillItem;
+import net.leo.herotech.item.custom.ModTiers;
 import net.leo.herotech.item.custom.ScannerItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -25,6 +26,14 @@ public class ModItems {
 
     public static final RegistryObject<Item> DRILL = ITEMS.register("drill",
             () -> new DrillItem(ModTiers.DRILL, 0, 0f,
+                    new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)));
+
+    public static final RegistryObject<Item> DIAMOND_DRILL = ITEMS.register("diamond_drill",
+            () -> new DrillItem(ModTiers.DIAMOND_DRILL, 0, 0f,
+                    new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)));
+
+    public static final RegistryObject<Item> ADVANCED_DIAMOND_DRILL = ITEMS.register("advanced_diamond_drill",
+            () -> new DrillItem(ModTiers.ADVANCED_DIAMOND_DRILL, 0, 0f,
                     new Item.Properties().tab(ModItemGroup.HEROTECH_TAB)));
 
     public static final RegistryObject<Item> SCANNER = ITEMS.register("scanner",
--- src/main/java/net/leo/herotech/item/custom/ModTiers.java
+++ src/main/java/net/leo/herotech/item/custom/ModTiers.java
@@ -1,4 +1,4 @@
-package net.leo.herotech.item;
+package net.leo.herotech.item.custom;
 
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -7,6 +7,10 @@ import net.minecraftforge.common.ForgeTier;
 public class ModTiers {
     public static final ForgeTier DRILL = new ForgeTier(2, 1000, 7f, 0f,
             0, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.EMPTY);
+
     public static final ForgeTier DIAMOND_DRILL = new ForgeTier(3, 1500, 10f, 0f,
+            0, BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.EMPTY);
+
+    public static final ForgeTier ADVANCED_DIAMOND_DRILL = new ForgeTier(4, 2000, 13f, 0f,
             0, BlockTags.NEEDS_DIAMOND_TOOL, () -> Ingredient.EMPTY);
 }
